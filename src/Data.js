b78b29ee259ca652c92773da8429d0871907d4a3
Upasana112 <97512762+Upasana112@users.noreply.github.com>
2023-06-21 03:46:56 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,59 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual T-Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/1-2.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 90,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/1-4.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Crop Top",
+    price: 40,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/1-8.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Casual T-shirt",
+    price: 30,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/1-5.jpg",
+  },
+  {
+    id: "thiecbawdjksadjk",
+    name: "Casual T-shirt",
+    price: 40,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/1-3.jpg",
+  },
+  {
+    id: "iuertrywebncdjksadjk",
+    name: "Casual T-Shirt",
+    price: 40,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/1-9.jpg",
+  },
+  {
+    id: "thierytbvcbvzdhadjk",
+    name: "Casual T-Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/1-10.jpg",
+  },
+  {
+    id: "trfoiwfcnbcawdjksadjk",
+    name: "Casual T-Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/1-11.jpg",
+  },
+ 
+];
